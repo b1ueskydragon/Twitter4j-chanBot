dae54ee4fb76e511fac6e94e26537913341b6bc3
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-08-21 15:11:44 +0900

    Add Keys class sample

---
+++ src/jp/keys/Keys_sample.java
@@ -0,0 +1,23 @@
+package jp.keys;
+
+
+public enum Keys_sample {
+
+    CONSUMERKEY(""),
+    CONSUMERSECRET(""),
+    ACCESSTOKEN(""),
+    ACCESSTOKENSECRET(""),
+
+    ME(""); // 自分の ID (@を除く)
+
+    private String value;
+    Keys_sample(String value){
+        this.value = value;
+    }
+
+    @Override
+    public String toString(){
+        return  value;
+    }
+
+}
