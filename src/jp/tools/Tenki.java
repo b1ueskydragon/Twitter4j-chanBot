25630fafac1266443798fec22f44e5a55460e1f7
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-08-28 23:02:27 +0900

    あしたのバグ修正

--- src/jp/tools/Tenki.java
+++ src/jp/tools/Tenki.java
@@ -5,7 +5,7 @@ import jp.keys.Keys;
 public class Tenki {
     public static String tenki(String targetMess) {
         String day = "今日"; // default
-        if (targetMess.contains("明日")) day = "明日"; // 単語分析・・?
+        if (targetMess.contains("明日") || targetMess.contains("あした")) day = "明日"; // 単語分析・・?
         String TENKI_URL =
                 Keys.TENKEY.toString();
         return day + "の天気を検索 " + TENKI_URL;
