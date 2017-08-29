178ad000ca04bb1bb1f32d02249691176def43f6
ちゃーん(こそこそVer) <dragoalie@gmail.com>
2017-08-30 02:23:01 +0900

    Fix Indent

--- src/jp/chan/ChanBotForReplyTrue.java
+++ src/jp/chan/ChanBotForReplyTrue.java
@@ -18,68 +18,70 @@ public class ChanBotForReplyTrue {
         twitter.setOAuthConsumer(Keys.CONSUMERKEY.toString(), Keys.CONSUMERSECRET.toString());
         twitter.setOAuthAccessToken(new AccessToken(Keys.ACCESSTOKEN.toString(), Keys.ACCESSTOKENSECRET.toString()));
 
-        while (true) try {
-            /**
-             * 自分とのやりとり
-             */
-            ResponseList<Status> statuses = twitter.getUserTimeline(); // 全タイムライン読み込み
-            List<String> idList = new ArrayList<>(); // ターゲットさんのつぶやきID
-            List<String> messList = new ArrayList<>(); // ターゲットさんがつぶやいた内容
-
-            StatusFilter(statuses, idList, messList);
-
-            BigInteger target = new BigInteger(idList.get(0)); // 一番最新のid取得
-            System.out.println(target + " :: " + messList.get(0));
-
-            /**
-             * Who's the target, ME or not.
-             */
-            String annoTarget =
-                    messList.get(0).substring(1, 2).equals("@")
-                            ? messList.get(0).split(" ")[0].substring(2) : Keys.ME.toString();
-
-            // TODO メンション -> 自つぶやき -> bot で返信する時、annoTarget 再設定問題 (行ナンバーずらし)
-
-            /**
-             * target is not ME
-             */
-            if (!annoTarget.equals(Keys.ME.toString())) {
-                // 変数入れ替え
-                statuses = twitter.getMentionsTimeline(); // メンションのみ読み込み
-                idList = new ArrayList<>();
-                messList = new ArrayList<>();
+        while (true) {
+            try {
+                /**
+                 * 自分とのやりとり
+                 */
+                ResponseList<Status> statuses = twitter.getUserTimeline(); // 全タイムライン読み込み
+                List<String> idList = new ArrayList<>(); // ターゲットさんのつぶやきID
+                List<String> messList = new ArrayList<>(); // ターゲットさんがつぶやいた内容
 
                 StatusFilter(statuses, idList, messList);
 
-                target = new BigInteger(idList.get(0));
+                BigInteger target = new BigInteger(idList.get(0)); // 一番最新のid取得
                 System.out.println(target + " :: " + messList.get(0));
+
+                /**
+                 * Who's the target, ME or not.
+                 */
+                String annoTarget =
+                        messList.get(0).substring(1, 2).equals("@")
+                                ? messList.get(0).split(" ")[0].substring(2) : Keys.ME.toString();
+
+                // TODO メンション -> 自つぶやき -> bot で返信する時、annoTarget 再設定問題 (行ナンバーずらし)
+
+                /**
+                 * target is not ME
+                 */
+                if (!annoTarget.equals(Keys.ME.toString())) {
+                    // 変数入れ替え
+                    statuses = twitter.getMentionsTimeline(); // メンションのみ読み込み
+                    idList = new ArrayList<>();
+                    messList = new ArrayList<>();
+
+                    StatusFilter(statuses, idList, messList);
+
+                    target = new BigInteger(idList.get(0));
+                    System.out.println(target + " :: " + messList.get(0));
+                }
+
+                int idx = 0;
+                String targetMess = messList.get(idx);
+                MessageFilter messageFilter = new MessageFilter();
+                String botReply = messageFilter.makeReply(targetMess);
+
+                if (targetMess.contains(FROMBOT)) {
+                    System.out.println("まだかなー？");
+                    Thread.sleep(10000);
+                } else {
+                    System.out.println("きたきたー！");
+                    // 更新される内容をセット
+                    StatusUpdate statusUpdate = new StatusUpdate("@" + annoTarget + " " + botReply + FROMBOT);
+                    // 画像がある場合
+                    if (messageFilter.getPicFlag()) MessageFilter.getInagon(statusUpdate);
+                    // 更新
+                    twitter.updateStatus(statusUpdate.inReplyToStatusId(target.longValue()));
+
+                    System.out.println("yes ok !");
+                    Thread.sleep(20000);
+                }
+
+            } catch (TwitterException e) {
+                System.out.println("no !" + e.getMessage());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-
-            int idx = 0;
-            String targetMess = messList.get(idx);
-            MessageFilter messageFilter = new MessageFilter();
-            String botReply = messageFilter.makeReply(targetMess);
-
-            if (targetMess.contains(FROMBOT)) {
-                System.out.println("まだかなー？");
-                Thread.sleep(10000);
-            } else {
-                System.out.println("きたきたー！");
-                // 更新される内容をセット
-                StatusUpdate statusUpdate = new StatusUpdate("@" + annoTarget + " " + botReply + FROMBOT);
-                // 画像がある場合
-                if (messageFilter.getPicFlag()) MessageFilter.getInagon(statusUpdate);
-                // 更新
-                twitter.updateStatus(statusUpdate.inReplyToStatusId(target.longValue()));
-
-                System.out.println("yes ok !");
-                Thread.sleep(20000);
-            }
-
-        } catch (TwitterException e) {
-            System.out.println("no !" + e.getMessage());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
     }
 
